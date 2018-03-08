209a9a40b6500511d2640b13f60dab0f29a9834f
gloverac08 <glover.amy@gmail.com>
2018-03-08 16:02:45 -0600

    Make it so new subcomments show after submitting them

--- app/components/subcomments.js
+++ app/components/subcomments.js
@@ -3,35 +3,60 @@ angular.module('app')
     bindings: {
       comment: '<',
       userid: '<',
-      postid: '<'
+      postid: '<',
+      userid: '<'
     },
     controller: function (subcommentsService) {
       this.subcomments = null;
       this.showSubcomments = false;
+      this.submessage = '';
+      this.replyClicked = false;
 
-      // this.$onInit = () => {
-      //   console.log('inited');
-      //   console.log('this.comment', this.comment);
-      //   console.log('this.userId:', this.userid);
-      //   console.log('this.comment.comment_id', this.comment.comment_id)
-      //   subcommentsService.getSubcomments(this.comment.comment_id, (data) => {
-      //     console.log('data from getSubcomments:', data);
-      //     this.subcomments = data;
-      //   });
-      // }
-      // this.toggleSubcomments = function() {
-      //   console.log('clicked');
-      //   this.showSubcomments = !this.showSubcomments;
-      // }
+
+      this.submitSubcomment = (isValid) => {
+        console.log('isValid from submitSubcomment:', isValid);
+        if (isValid) {
+
+          let subcommentObj = {
+            user_id: this.userid,
+            post_id: this.postid,
+            comment_id: this.comment.comment_id,
+            submessage: this.submessage
+          }
+          console.log('subcommentObj:', subcommentObj);
+          subcommentsService.submitNewSubcomment(subcommentObj, (data) => {
+            console.log('insubcommentsService data:', data);
+            this.submessage = '';
+            this.showSubcomments = false;
+            this.getSubcomments();
+          });
+        }
+      };
+
+
+      this.showReplyForm = () => {
+        console.log('clicked');
+        this.replyClicked = !this.replyClicked;
+        console.log('this.replyClicked:', this.replyClicked)
+      }
+
+
+      this.toggleSubcomments = () => {
+        this.showSubcomments = !this.showSubcomments;
+      };
 
       this.getSubcomments = () => {
-        console.log('clicked');
-        subcommentsService.getSubcomments(this.comment.comment_id, (data) => {
-          console.log('data from getSubcomments:', data);
-          this.subcomments = data;
-          this.showSubcomments = true;
-        });
-      }
+        if (this.showSubcomments) {
+          this.showSubcomments = false;
+        } else {
+          console.log('clicked');
+          subcommentsService.getSubcomments(this.comment.comment_id, (data) => {
+            console.log('data from getSubcomments:', data);
+            this.subcomments = data;
+            this.showSubcomments = true;
+          });
+        }
+      };
     },
 
      templateUrl: 'templates/subcomments.html'
