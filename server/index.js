7b06de96b81a11b75effd93fb87a8b0c6d7546ab
Marci McClenon <mccmarci@gmail.com>
2018-03-10 14:01:08 -0600

    fixes autologin

--- server/index.js
+++ server/index.js
@@ -180,7 +180,7 @@ app.post('/login', async (req, res) => {
     if (bcrypt.compareSync(req.body.password, user.password)) {
 
       req.session.loggedIn = true;
-      req.session.user = user;
+      req.session.user_id = user.user_id;
 
       res.status(200).json({
         user_id: user.user_id,
@@ -197,9 +197,10 @@ app.post('/login', async (req, res) => {
 
 
 
-app.post('/autoLogin', (req,res) => {
+app.post('/autoLogin', async (req,res) => {
   if (req.session.loggedIn === true) {
-    res.send(req.session.user);
+    let user = await db.getUser(req.session.user_id)
+    res.json(user);
   } else {
     res.end();
   }
